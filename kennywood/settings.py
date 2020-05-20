5cc0584d4f3555cab248c81cca06a54623e950dc
keithrpotempa <keithrpotempa@gmail.com>
2020-05-20 13:31:40 -0500

    modified settings to whitelist react app

--- kennywood/settings.py
+++ kennywood/settings.py
@@ -48,9 +48,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        # When Django auth works...
-        # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        # 'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
@@ -68,8 +67,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8080',
-    'http://127.0.0.1:8080'
+    'http://localhost:8080', # Django app
+    'http://127.0.0.1:8080', # Django app
+    'http://localhost:3000', # React app
+    'http://127.0.0.1:3000' # React app
 )
 
 ROOT_URLCONF = 'kennywood.urls'
