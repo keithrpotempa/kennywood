b5615bd8166862833e01f525d3bda5e1be3bd746
keithrpotempa <keithrpotempa@gmail.com>
2020-06-01 15:00:33 -0500

    attraction and itineraries testing completed

--- kennywoodapi/tests/testattraction.py
+++ kennywoodapi/tests/testattraction.py
@@ -23,90 +23,85 @@ class TestAttractions(TestCase):
             theme="Integration tests"
         )
       
-        # Create an itinerary
+        # Then create an itinerary
         new_attraction = {
             "name": "ATTRACTION",
             "area_id": 1
         }
 
-        # Use the client to make the HTTP POST request and store the response
         response = self.client.post(
             reverse('attraction-list'), new_attraction, HTTP_AUTHORIZATION='Token ' + str(self.token)
         )
 
-        # Assert: Does the status code of the HTTP response indicate that the it was successful?
         self.assertEqual(response.status_code, 200)
 
-        # Query the table to see if there's one attraction instance in there. Since we are testing a POST request, we use the ORM to make sure the item was created in the database. Remember, the tests are run against a testing database that initially has no data.
-        # Assert: Is there one new itinerary in the database?
         self.assertEqual(Attraction.objects.count(), 1)
 
-        # Assert: Is the attraction in the database the one we just created?
         self.assertEqual(Attraction.objects.get().name, new_attraction["name"])
     
-    @unittest.skip("haven't built yet")
-    def testGetitinerary(self):
-        new_attraction = itinerary.objects.create(
+    def testGetAttraction(self):
+        new_parkarea = ParkArea.objects.create(
             name="Test Park Area",
             theme="Integration tests"
         )
+      
+        new_attraction = Attraction.objects.create(
+            name="Test Attraction Area",
+            area_id=1
+        )
 
-        # Now we can grab all the itinerarys (meaning the one we just created) from the db
         response = self.client.get(
-            reverse('itinerary-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
+            reverse('attraction-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
 
-        # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
 
-        # There's just one product in our testing db, so our collection of itinerarys that we get back as a response should have one itinerary.
         self.assertEqual(len(response.data), 1)
-        # Remember, response.data is the Python serialized data used to render the JSON, while response.content is the JSON itself.
-
-        # First, test the contents of the data before serialization
         self.assertEqual(response.data[0]["id"], 1)
         self.assertEqual(response.data[0]["name"], new_attraction.name)
 
-        # Then, test the actual rendered content as the client would receive it.
-        # .encode converts from unicode to utf-8. Don't get hung up on this. It's just how we can compare apples to apples
         self.assertIn(new_attraction.name.encode(), response.content)
         
-    @unittest.skip("haven't built yet")
-    def testDeleteitinerary(self):
-        new_attraction = itinerary.objects.create(
+    def testDeleteAttraction(self):
+        new_parkarea = ParkArea.objects.create(
             name="Test Park Area",
             theme="Integration tests"
         )
+      
+        new_attraction = Attraction.objects.create(
+            name="Test Attraction Area",
+            area_id=1
+        )
 
-        # Delete a itinerary. As shown in our post and get tests above, new_attraction
-        # will be the only itinerary in the database, and will have an id of 1
         response = self.client.delete(
-            reverse('itinerary-detail', kwargs={'pk': 1}), HTTP_AUTHORIZATION='Token ' + str(self.token))
+            reverse('attraction-detail', kwargs={'pk': 1}), HTTP_AUTHORIZATION='Token ' + str(self.token))
 
-        # Check that the response is 204.
         self.assertEqual(response.status_code, 204)
 
-        # Confirm that the itinerary is NOT in the database, which means there is nothing in the itinerary table.
         response = self.client.get(
-            reverse('itinerary-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
+            reverse('attraction-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
         self.assertEqual(len(response.data), 0)
         
-    @unittest.skip("haven't built yet")
-    def testEdititinerary(self):
-        new_attraction = itinerary.objects.create(
+    def testEditAttraction(self):
+        new_parkarea = ParkArea.objects.create(
             name="Test Park Area",
             theme="Integration tests"
         )
-
+            
+        new_attraction = Attraction.objects.create(
+            name="Test Attraction Area",
+            area_id=1
+        )
+        
         # Create new updated parkare.
-        updated_itinerary = {
-            "name": "Updated Test Park Area",
-            "theme": "Integration tests"
+        updated_attraction = {
+            "name": "Updated Test Attraction",
+            "area_id": 1
         }
 
-        # Update the itinerary in the db
+        # Update the attraction in the db
         response = self.client.put(
-            reverse('itinerary-detail', kwargs={'pk': 1}),
-            updated_itinerary,
+            reverse('attraction-detail', kwargs={'pk': 1}),
+            updated_attraction,
             content_type='application/json',
             HTTP_AUTHORIZATION='Token ' + str(self.token)
         )
@@ -114,13 +109,13 @@ class TestAttractions(TestCase):
         # Assert that the PUT returns the expected 204 status
         self.assertEqual(response.status_code, 204)
 
-        # Get the itinerary again, it should now be the updated itinerary.
+        # Get the attraction again, it should now be the updated attraction.
         response = self.client.get(
-            reverse('itinerary-detail', kwargs={'pk': 1}), HTTP_AUTHORIZATION='Token ' + str(self.token)
+            reverse('attraction-detail', kwargs={'pk': 1}), HTTP_AUTHORIZATION='Token ' + str(self.token)
         )
 
         # Assert that the name has been updated
-        self.assertEqual(response.data["name"], updated_itinerary["name"])
+        self.assertEqual(response.data["name"], updated_attraction["name"])
 
 
 if __name__ == '__main__':
--- kennywoodapi/tests/testitinerary.py
+++ kennywoodapi/tests/testitinerary.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from kennywoodapi.models import Itinerary, Customer, Attraction
+from kennywoodapi.models import Itinerary, Customer, Attraction, ParkArea
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 import unittest
@@ -8,7 +8,6 @@ import unittest
 
 class TestItineraries(TestCase):
     # Set up all data that will be needed to excute all the tests in the test file.
-    @unittest.skip("TODO: Return to this after attraction testing is built")
     def setUp(self):
         self.username = 'TestUser'
         self.password = 'Test123'
@@ -17,8 +16,17 @@ class TestItineraries(TestCase):
         self.token = Token.objects.create(user=self.user)
         self.customer = Customer.objects.create(user_id=1, family_members=9)
 
-    @unittest.skip("TODO: Return to this after attraction testing is built")
     def testPostItinerary(self):
+        new_parkarea = ParkArea.objects.create(
+            name="Test Park Area",
+            theme="Integration tests"
+        )
+      
+        new_attraction = Attraction.objects.create(
+            name="Test Attraction Area",
+            area_id=1
+        )
+      
         # Create an itinerary
         new_itinerary = {
             "attraction_id": 1,
@@ -36,42 +44,54 @@ class TestItineraries(TestCase):
 
         # Query the table to see if there's one itinerary instance in there. Since we are testing a POST request, we use the ORM to make sure the item was created in the database. Remember, the tests are run against a testing database that initially has no data.
         # Assert: Is there one new itinerary in the database?
-        self.assertEqual(itinerary.objects.count(), 1)
+        self.assertEqual(Itinerary.objects.count(), 1)
 
         # Assert: Is the itinerary in the database the one we just created?
-        self.assertEqual(itinerary.objects.get().attraction_id, new_itinerary["attraction_id"])
+        self.assertEqual(Itinerary.objects.get().attraction_id, new_itinerary["attraction_id"])
     
-    @unittest.skip("haven't built yet")
     def testGetitinerary(self):
-        new_itinerary = itinerary.objects.create(
+        new_parkarea = ParkArea.objects.create(
             name="Test Park Area",
             theme="Integration tests"
         )
+      
+        new_attraction = Attraction.objects.create(
+            name="Test Attraction Area",
+            area_id=1
+        )
+      
+        new_itinerary = Itinerary.objects.create(
+            attraction_id=1,
+            customer_id=1,
+            starttime=1
+        )
 
-        # Now we can grab all the itinerarys (meaning the one we just created) from the db
         response = self.client.get(
             reverse('itinerary-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
 
-        # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
-
-        # There's just one product in our testing db, so our collection of itinerarys that we get back as a response should have one itinerary.
         self.assertEqual(len(response.data), 1)
-        # Remember, response.data is the Python serialized data used to render the JSON, while response.content is the JSON itself.
-
-        # First, test the contents of the data before serialization
         self.assertEqual(response.data[0]["id"], 1)
-        self.assertEqual(response.data[0]["name"], new_itinerary.name)
+        self.assertEqual(response.data[0]["attraction_id"], new_itinerary.attraction_id)
 
-        # Then, test the actual rendered content as the client would receive it.
-        # .encode converts from unicode to utf-8. Don't get hung up on this. It's just how we can compare apples to apples
-        self.assertIn(new_itinerary.name.encode(), response.content)
+        # There are no characters, so this was removed from the process:
+        # self.assertIn(new_itinerary.attraction_id.encode(), response.content)
         
-    @unittest.skip("haven't built yet")
-    def testDeleteitinerary(self):
-        new_itinerary = itinerary.objects.create(
+    def testDeleteItinerary(self):
+        new_parkarea = ParkArea.objects.create(
             name="Test Park Area",
             theme="Integration tests"
+        )
+      
+        new_attraction = Attraction.objects.create(
+            name="Test Attraction Area",
+            area_id=1
+        )
+      
+        new_itinerary = Itinerary.objects.create(
+            attraction_id=1,
+            customer_id=1,
+            starttime=1
         )
 
         # Delete a itinerary. As shown in our post and get tests above, new_itinerary
@@ -87,17 +107,28 @@ class TestItineraries(TestCase):
             reverse('itinerary-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
         self.assertEqual(len(response.data), 0)
         
-    @unittest.skip("haven't built yet")
-    def testEdititinerary(self):
-        new_itinerary = itinerary.objects.create(
+    def testEditItinerary(self):
+        new_parkarea = ParkArea.objects.create(
             name="Test Park Area",
             theme="Integration tests"
+        )
+      
+        new_attraction = Attraction.objects.create(
+            name="Test Attraction Area",
+            area_id=1
+        )
+      
+        new_itinerary = Itinerary.objects.create(
+            attraction_id=1,
+            customer_id=1,
+            starttime=1
         )
 
         # Create new updated parkare.
         updated_itinerary = {
-            "name": "Updated Test Park Area",
-            "theme": "Integration tests"
+            "attraction_id": 1,
+            "customer_id": 1,
+            "starttime": 5
         }
 
         # Update the itinerary in the db
@@ -117,7 +148,7 @@ class TestItineraries(TestCase):
         )
 
         # Assert that the name has been updated
-        self.assertEqual(response.data["name"], updated_itinerary["name"])
+        self.assertEqual(response.data["starttime"], updated_itinerary["starttime"])
 
 
 if __name__ == '__main__':
